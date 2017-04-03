530c5fc985fe1ff2f3a03334e992edb010a82951
MrGuppy <marksturtz62@gmail.com>
2017-04-04 08:42:16 +1000

    First Commit

---
+++ VecLibrary/Vector2.h
@@ -0,0 +1,23 @@
+#pragma once
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(float x, float y);
+	~Vector2();
+
+	Vector2 operator+(const Vector2& rhs);
+	Vector2 operator*(const float rhs);
+	Vector2 operator-(const Vector2& rhs);
+	Vector2 operator/(const Vector2& rhs);
+	Vector2 operator+=(const Vector2& rhs);
+	float dot(const Vector2 rhs);
+	float normalize();
+	float magnitude();
+
+	float x;
+	float y;
+};
+
+Vector2 operator*(const float lhs, const Vector2& rhs);
+Vector2 operator/(const float lhs, const Vector2& rhs);
---
+++ VecLibrary/Vector3.h
@@ -0,0 +1,26 @@
+#pragma once
+class Vector3
+{
+public:
+	Vector3();
+	Vector3(float x, float y, float z);
+	~Vector3();
+
+	float x;
+	float y;
+	float z;
+
+	Vector3 operator+(const Vector3& rhs);
+	Vector3 operator*(const float rhs);
+	Vector3 operator-(const Vector3& rhs);
+	Vector3 operator/(const Vector3& rhs);
+	Vector3 operator+=(const Vector3& rhs);
+	float dot(const Vector3 rhs);
+	float normalize();
+	float magnitude();
+
+
+};
+
+Vector3 operator*(const float lhs, const Vector3& rhs);
+Vector3 operator/(const float lhs, const Vector3& rhs);
